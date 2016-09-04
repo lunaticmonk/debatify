50f7fbbe58144642ab106cb62184edaf0b55e128
Sumedh Nimkarde <blesssumedh@gmail.com>
2016-09-04 21:52:28 +0530

    added side Navbar

---
+++ assets/js/sideNav.js
@@ -0,0 +1,10 @@
+ // Initialize collapse button
+ (function(){
+  $(".button-collapse").sideNav({
+  	menuWidth : 300,
+  	edge : 'top'
+  });
+ })();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
+        
