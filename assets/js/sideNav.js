869df6127d0cec017bec8e986442824a1bc8994c
Sumedh Nimkarde <blesssumedh@gmail.com>
2016-09-10 20:20:39 +0530

    login popup added

--- assets/js/sideNav.js
+++ assets/js/sideNav.js
@@ -4,7 +4,22 @@
   	menuWidth : 300,
   	edge : 'top'
   });
+
+
+	var loginpanel = document.querySelector('#loginpanel');
+	console.log(loginpanel);
+	loginpanel.style.visibility = "hidden";
+
+	var signinbtn = document.querySelector('#signinbtn');
+	signinbtn.addEventListener('click',function(){
+		console.log('hello');
+		loginpanel.style.visibility = "visible";
+		// loginpanel.style.transition = "opacity 5s";
+		// loginpanel.style.opacity = "1";
+	});
  })();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
         
+
+//Login panel
