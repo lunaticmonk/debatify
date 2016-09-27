cff934dc9558f377ed64dbd6b0721971e8b16fb0
Sumedh Nimkarde <blesssumedh@gmail.com>
2016-09-28 01:40:49 +0530

    modified templates

--- static/assets/js/sideNav.js
+++ static/assets/js/sideNav.js
@@ -9,14 +9,26 @@
 	var loginpanel = document.querySelector('#loginpanel');
 	console.log(loginpanel);
 	loginpanel.style.visibility = "hidden";
+	var signuppanel = document.querySelector('#signup');
+	signuppanel.style.visibility = "hidden";
 
 	var signinbtn = document.querySelector('#signinbtn');
+	var signupbtn  = document.querySelector('#signupbtn');
+
 	signinbtn.addEventListener('click',function(){
 		console.log('hello');
 		loginpanel.style.visibility = "visible";
 	var closebtn = document.querySelector('#closebtn');
 		closebtn.addEventListener('click',function(){
 			loginpanel.style.visibility = 'hidden';
+		});
+	});
+
+	signupbtn.addEventListener('click',function(){
+		signuppanel.style.visibility = "visible";
+	var closesignup = document.querySelector('#closesignup');
+		closesignup.addEventListener('click',function(){
+			signuppanel.style.visibility = 'hidden';
 		});
 	});
 
