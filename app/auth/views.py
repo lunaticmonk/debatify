d33b63afe62ab40085e36c56792016c573411484
Sumedh Nimkarde <blesssumedh@gmail.com>
2016-10-02 17:03:50 +0530

    frontend ready

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,6 +2,10 @@ from flask import Blueprint,render_template
 
 admin = Blueprint('auth',__name__)
 
+@admin.route('/')
+def index():
+	return render_template('index.html')
+
 @admin.route('/login')
 def login():
 	return render_template('login.html')
@@ -9,4 +13,16 @@ def login():
 @admin.route('/register')
 def register():
 	return render_template('register.html')
+
+@admin.route('/terms')
+def terms():
+	return render_template('terms.html')
+
+@admin.route('/privacy')
+def privacy():
+	return render_template('privacy.html')
+
+@admin.route('/about')
+def about():
+	return render_template('about.html')
 
