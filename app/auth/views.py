053c259a459af1535468327f1e2e4278b2f9c9bb
Sumedh Nimkarde <blesssumedh@gmail.com>
2016-10-02 13:26:01 +0530

    login and sign up ready

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,8 +2,11 @@ from flask import Blueprint,render_template
 
 admin = Blueprint('auth',__name__)
 
-@admin.route('/home')
-def home():
-	return '<p>Hello there!</p>'
+@admin.route('/login')
+def login():
+	return render_template('login.html')
 
+@admin.route('/register')
+def register():
+	return render_template('register.html')
 
